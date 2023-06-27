382781b9619f8cce4c3e7c5cac759f8d1ff37415
Daniil <d_tareev@mail.ru>
2023-06-28 00:02:36 +0300

    bg

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Header from "../components/Header/Header";
+
+const App = () => {
+  return (
+    <Container>
+      <Header></Header>
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+
+export default App;
