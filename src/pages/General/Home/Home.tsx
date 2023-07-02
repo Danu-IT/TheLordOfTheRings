84c443a6f08bbf128d5aec7cecb4077e2e458f2d
Тареев Даниил <73935646+Danu-IT@users.noreply.github.com>
2023-07-03 00:46:08 +0300

    Revert "Pagination"

--- src/pages/General/Home/Home.tsx
+++ src/pages/General/Home/Home.tsx
@@ -1,26 +1,19 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import { useNavigate, useSearchParams } from "react-router-dom";
 
 import { useAppDispatch } from "../../../hooks/redux";
 import { changeUser } from "../../../store/slices/auth";
 import { auth } from "../../../firebase";
 import { ringsAPI } from "../../../services/RingsService";
 import Card from "../../../components/Card/Card";
-import Pagination from "../../../components/Pagination";
 
 const Home = () => {
-  const [searchParams] = useSearchParams();
-  const pageQuery = searchParams.get("page") || "1";
-
-  const [pageState, setPageState] = useState(Number(pageQuery));
-
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const { data } = ringsAPI.useGetCharactersQuery({ page: pageState });
+  const { data } = ringsAPI.useGetCharactersQuery(null);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -28,27 +21,18 @@ const Home = () => {
     });
   }, []);
 
-  useEffect(() => {
-    navigate(`?page=${pageState}`);
-  }, [pageState, setPageState]);
-
   return (
     <Container>
       <Cards>
-      {data?.docs &&
+        {data?.docs &&
           data.docs.map((el) => (
             <Card
               onClick={(id: string) => navigate(`/${id}`)}
               key={el.id}
               item={el}
             />
-         ))
-      }
+          ))}
       </Cards>
-      <Pagination
-        setPageState={setPageState}
-        pageState={pageState}
-        info={data}></Pagination>
     </Container>
   );
 };
@@ -56,9 +40,6 @@ const Home = () => {
 const Container = styled.div`
   max-width: 1300px;
   margin: 10px auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `;
 const Cards = styled.div`
   display: flex;
